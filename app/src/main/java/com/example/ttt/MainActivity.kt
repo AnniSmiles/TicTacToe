e46fbffdb1e4752aa4b70aea03be11a3076957b7
AnniSmiles <ana.katsitadze.1@btu.edu.ge>
2020-12-05 01:32:58 +0400

    first commit

---
+++ app/src/main/java/com/example/ttt/MainActivity.kt
@@ -0,0 +1,220 @@
+package com.example.ttt
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
+
+class MainActivity :  AppCompatActivity(), View.OnClickListener {
+    private lateinit var button1:Button
+    private lateinit var button2:Button
+    private lateinit var button3:Button
+    private lateinit var button4:Button
+    private lateinit var button5:Button
+    private lateinit var button6:Button
+    private lateinit var button7:Button
+    private lateinit var button8:Button
+    private lateinit var button9:Button
+
+    private lateinit var resetButton:Button
+
+    private var activePlayer = 1
+
+    private var counter=0
+
+    private var firstPlayer = ArrayList<Int>()
+    private var secondPlayer = ArrayList<Int>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
+    }
+    private fun init() {
+        button1=findViewById(R.id.button1)
+        button2=findViewById(R.id.button2)
+        button3=findViewById(R.id.button3)
+        button4=findViewById(R.id.button4)
+        button5=findViewById(R.id.button5)
+        button6=findViewById(R.id.button6)
+        button7=findViewById(R.id.button7)
+        button8=findViewById(R.id.button8)
+        button9=findViewById(R.id.button9)
+
+        resetButton=findViewById(R.id.resetGame)
+
+        button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
+        button4.setOnClickListener(this)
+        button5.setOnClickListener(this)
+        button6.setOnClickListener(this)
+        button7.setOnClickListener(this)
+        button8.setOnClickListener(this)
+        button9.setOnClickListener(this)
+
+
+    }
+    override fun onClick(clickedView:View?) {
+        if (clickedView is Button) {
+            var buttonNum = 0
+            when (clickedView.id) {
+                R.id.button1 -> buttonNum=1
+                R.id.button2 -> buttonNum=2
+                R.id.button3 -> buttonNum=3
+                R.id.button4 -> buttonNum=4
+                R.id.button5 -> buttonNum=5
+                R.id.button6 -> buttonNum=6
+                R.id.button7 -> buttonNum=7
+                R.id.button8 -> buttonNum=8
+                R.id.button9 -> buttonNum=9
+
+            }
+            if (buttonNum !=0) {
+                playGame(clickedView, buttonNum)
+            }
+        }
+    }
+
+    private fun playGame(clickedView: Button, buttonNum: Int) {
+           if (activePlayer == 1) {
+               clickedView.text = "X"
+
+               firstPlayer.add(buttonNum)
+               counter+=1
+               activePlayer=2
+           } else {
+               clickedView.text="0"
+
+               secondPlayer.add(buttonNum)
+               counter+=1
+               activePlayer=1
+           }
+        clickedView.isEnabled=false
+        check()
+    }
+
+    private fun check() {
+
+        var winnerPlayer = 0
+
+        if (firstPlayer.contains(1) && firstPlayer.contains(2) && firstPlayer.contains(3)) {
+            winnerPlayer = 1
+        }
+        if (secondPlayer.contains(1) && secondPlayer.contains(2) && secondPlayer.contains(3)) {
+            winnerPlayer = 2
+        }
+
+        if (firstPlayer.contains(4) && firstPlayer.contains(5) && firstPlayer.contains(6)) {
+            winnerPlayer = 1
+        }
+        if (secondPlayer.contains(4) && secondPlayer.contains(5) && secondPlayer.contains(6)) {
+            winnerPlayer = 2
+        }
+
+        if (firstPlayer.contains(7) && firstPlayer.contains(8) && firstPlayer.contains(9)) {
+            winnerPlayer = 1
+        }
+        if (secondPlayer.contains(7) && secondPlayer.contains(8) && secondPlayer.contains(9)) {
+            winnerPlayer = 2
+        }
+
+        if (firstPlayer.contains(1) && firstPlayer.contains(4) && firstPlayer.contains(7)) {
+            winnerPlayer = 1
+        }
+        if (secondPlayer.contains(1) && secondPlayer.contains(4) && secondPlayer.contains(7)) {
+            winnerPlayer = 2
+        }
+
+        if (firstPlayer.contains(2) && firstPlayer.contains(5) && firstPlayer.contains(8)) {
+            winnerPlayer = 1
+        }
+        if (secondPlayer.contains(2) && secondPlayer.contains(5) && secondPlayer.contains(8)) {
+            winnerPlayer = 2
+        }
+
+        if (firstPlayer.contains(3) && firstPlayer.contains(6) && firstPlayer.contains(9)) {
+            winnerPlayer = 1
+        }
+        if (secondPlayer.contains(3) && secondPlayer.contains(6) && secondPlayer.contains(9)) {
+            winnerPlayer = 2
+        }
+
+        if (firstPlayer.contains(1) && firstPlayer.contains(5) && firstPlayer.contains(9)) {
+            winnerPlayer = 1
+        }
+        if (secondPlayer.contains(1) && secondPlayer.contains(5) && secondPlayer.contains(9)) {
+            winnerPlayer = 2
+        }
+
+        if (firstPlayer.contains(3) && firstPlayer.contains(5) && firstPlayer.contains(7)) {
+            winnerPlayer = 1
+        }
+        if (secondPlayer.contains(3) && secondPlayer.contains(5) && secondPlayer.contains(7)) {
+            winnerPlayer = 2
+        }
+
+        if (winnerPlayer != 0) {
+            if (winnerPlayer == 1) {
+                Toast.makeText(this, "X wins!", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "0 wins!", Toast.LENGTH_LONG).show()
+            }
+            disableButtons();
+
+        }
+        if (winnerPlayer==0 && counter == 9){
+            Toast.makeText(this,"U are evenly matched!",Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+
+    private fun disableButtons(){
+        button1.isEnabled=false
+        button2.isEnabled=false
+        button3.isEnabled=false
+        button4.isEnabled=false
+        button5.isEnabled=false
+        button6.isEnabled=false
+        button7.isEnabled=false
+        button8.isEnabled=false
+        button9.isEnabled=false
+
+    }
+
+    fun resetButton(view: View) {
+        firstPlayer.clear()
+        secondPlayer.clear()
+        activePlayer=1
+        counter=0
+        button1.setText("")
+        button2.setText("")
+        button3.setText("")
+        button4.setText("")
+        button5.setText("")
+        button6.setText("")
+        button7.setText("")
+        button8.setText("")
+        button9.setText("")
+
+
+
+
+        button1.isEnabled=true
+        button2.isEnabled=true
+        button3.isEnabled=true
+        button4.isEnabled=true
+        button5.isEnabled=true
+        button6.isEnabled=true
+        button7.isEnabled=true
+        button8.isEnabled=true
+        button9.isEnabled=true
+    }
+
+
+
+}
